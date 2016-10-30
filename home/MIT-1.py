84f2988374628fab866b24a51e9538c165aba4df
B Eric Davis <feralmonkey@gmail.com>
2016-10-30 19:41:53 -0400

    Mucking

---
+++ home/MIT-1.py
@@ -0,0 +1,198 @@
+
+import logging
+import collections
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(funcName)s: %(lineno)d %(message)s')
+
+s = (1, 1)
+level = {s: 0}
+dirs = collections.defaultdict(str)
+
+
+def find_children(p, maze_map):
+    """Find adjacent nodes.  If a node is a 0 (a valid path) and has not had its level recorded
+    ret."""
+    logging.info('Finding Children of {}'.format(p))
+    child_list = []
+    curr_x = p[0]
+    curr_y = p[1]
+    logging.info('curr_x: {}, curr_y: {}'.format(curr_x, curr_y))
+    # check west
+    logging.info('WEST: curr_x-1, curr_y is {}'.format(maze_map[curr_x - 1][curr_y]))
+    if maze_map[curr_x - 1][curr_y] not in level and maze_map[curr_x - 1][curr_y] == 0:
+        logging.info('Adding ({}, {})'.format(curr_x - 1, curr_y))
+        child_list.append((curr_x - 1, curr_y))
+        dirs[(curr_x -1, curr_y)] = 'W'
+
+
+    # check south
+    logging.info('SOUTH: curr_x, curr_y+1 is {}'.format(maze_map[curr_x][curr_y + 1]))
+    if maze_map[curr_x][curr_y + 1] not in level and maze_map[curr_x][curr_y + 1] == 0:
+        logging.info('\tAdding ({}, {})'.format(curr_x, curr_y + 1))
+        child_list.append((curr_x, curr_y + 1))
+        dirs[(curr_x, curr_y+1 )] = 'S'
+
+    # check east
+    logging.info('EAST: curr_x+1, curr_y is {}'.format(maze_map[curr_x + 1][curr_y]))
+    if maze_map[curr_x + 1][curr_y] not in level and maze_map[curr_x + 1][curr_y] == 0:
+        logging.info('\tAdding ({}, {})'.format(curr_x + 1, curr_y))
+        child_list.append((curr_x + 1, curr_y))
+        dirs[(curr_x + 1, curr_y)] = 'E'
+
+    # check north
+    logging.info('NORTH: curr_x, curr_y-1 is {}'.format(maze_map[curr_x][curr_y-1]))
+    if maze_map[curr_x][curr_y - 1] not in level and maze_map[curr_x][curr_y - 1] == 0:
+        logging.info('\tAdding ({}, {})'.format(curr_x, curr_y - 1))
+        child_list.append((curr_x, curr_y - 1))
+        dirs[(curr_x, curr_y-1)] = 'N'
+
+    logging.info('Returning: {}'.format(child_list))
+    return child_list
+
+def checkio(maze_map):
+    # s = (1, 1)
+    # level = {s: 0}
+    parents = {s:
+                   {'parent': None,
+                    'direction': None}
+               }
+    i = 1
+    frontier = [s]  # level i - 1
+    print_maze(maze_map)
+    while frontier:
+        next = []  # level i
+        for u in frontier:
+            for v in find_children(u, maze_map):
+                if v not in level:
+                    level[v] = i
+                    parents[v] = u
+                    next.append(v)
+        frontier = next
+        i += 1
+
+    node = (10,10)
+    path = []
+
+    while node != (1, 1):
+        parent = parents[node]
+        path.append(dirs[parent])
+        node = parent
+
+    path.reverse()
+    logging.info('path = {}'.format(path))
+
+    final_path = ''
+    for d in path:
+        final_path += d
+
+    return final_path
+
+    
+def print_maze(maze):
+    for row in maze:
+        logging.info(row)
+
+
+if __name__ == '__main__':
+    # This code using only for self-checking and not necessary for auto-testing
+    def check_route(func, labyrinth):
+        MOVE = {"S": (1, 0), "N": (-1, 0), "W": (0, -1), "E": (0, 1)}
+        # copy maze
+        route = func([row[:] for row in labyrinth])
+        pos = (1, 1)
+        goal = (10, 10)
+        for i, d in enumerate(route):
+            move = MOVE.get(d, None)
+            if not move:
+                logging.info("Wrong symbol in route")
+                return False
+            pos = pos[0] + move[0], pos[1] + move[1]
+            if pos == goal:
+                return True
+            if labyrinth[pos[0]][pos[1]] == 1:
+                logging.info("Player in the pit")
+                return False
+        logging.info("Player did not reach exit")
+        return False
+
+
+    # These assert are using only for self-testing as examples.
+    assert check_route(checkio, [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1],
+        [1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "First maze"
+    assert check_route(checkio, [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "Empty maze"
+    assert check_route(checkio, [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "Up and down maze"
+    assert check_route(checkio, [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "Dotted maze"
+    assert check_route(checkio, [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+        [1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1],
+        [1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1],
+        [1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+        [1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "Need left maze"
+    assert check_route(checkio, [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+        [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+        [1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "The big dead end."
+    print("The local tests are done.")
