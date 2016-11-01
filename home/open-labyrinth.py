4d9f4fabfc830772718c635dc29f04f108d508d5
Eric Davis <brian.davis@elsevier.com>
2016-11-01 17:47:28 -0400

    Submitted and cleaned up

--- home/open-labyrinth.py
+++ home/open-labyrinth.py
@@ -1,99 +1,75 @@
-# The labyrinth has no walls, but bushes surround the path on each side.
-# If a players move into a bush, they lose. The labyrinth is presented as a matrix (a list of lists): 1 is a bush
-#  and 0 is part of the path. The labyrinth's size is 12 x 12 and the outer cells are also bushes.
-# Players start at cell (1,1). The exit is at cell (10,10). You need to find a route through the labyrinth.
-# Players can move in only four directions--South (down [1,0]), North (up [-1,0]), East (right [0,1]),
-#  West (left [0, -1]). The route is described as a string consisting of different
-# characters: "S"=South, "N"=North, "E"=East, and "W"=West.
+import collections
 
-#Your code here
-#You can import some modules or create additional functions
-import Queue
+s = (1, 1)
+
+
+def find_children(p, maze_map, bfs):
+    """Find adjacent nodes.  If a node is a 0 (a valid path) and has not been seen
+    add it to the list of children."""
+    child_list = []
+    moves = {(1, 0): "S", (-1, 0): "N", (0, -1): "W", (0, 1): "E"}
+
+    for move in moves:
+        check_x = p[0] + move[0]
+        check_y = p[1] + move[1]
+        node = (check_x, check_y)
+        if (check_x, check_y) not in bfs and maze_map[check_x][check_y] == 0:
+            child_list.append((check_x, check_y))
+            bfs[node]['parent'] = p
+            bfs[node]['move'] = moves[move]
+    return child_list
 
 
 def checkio(maze_map):
-    start = maze_map[1][1]
-    goal = maze_map[10][10]
-    last_pos = maze_map[1][1]
+    bfs = collections.defaultdict(lambda: collections.defaultdict(dict))
+    bfs[s]['parent'] = None
+    bfs[s]['move'] = None
+    i = 1
+    frontier = [s]  # level i - 1
 
-    # while we're not at the goal
-    if __name__ == '__main__':
-        # Breadth - First - Search(Graph, root):
-        def bfs(map):
-            pos = start
+    while frontier:
+        next_batch = []  # level i
+        for u in frontier:
+            for v in find_children(u, maze_map, bfs):
+                next_batch.append(v)
+        frontier = next_batch
+        i += 1
 
+    node = (10,10)
+    path = []
 
-            # for each node n in Graph:
-            for n in maze_map:
-            #     n.distance = INFINITY
-                n.distance = -1
-            #     n.parent = NIL
-                n.parent = None
-            #   create empty queue Q
-                queue = Queue.Queue()
-            #
-            #   root.distance = 0
-                pos.distance = 0
-            #   Q.enqueue(root)
-                queue.put(pos)
+    while node != (1, 1):
+        parent = bfs[node]['parent']
+        path.append(bfs[node]['move'])
+        node = parent
 
-            #   while Q is not empty:
-                while queue:
-            #       current = Q.dequeue()
-                    current = queue.get()
+    path.reverse()
 
-                    adjacent_nodes = get_adjacent_nodes(maze_map, n[x], n[y])
-            #       for each node n that is adjacent to current:
-                    for node in adjacent_nodes:
-            #           if n.distance == INFINITY:
-                        if 1:
-                            pass
-            #               n.distance = current.distance + 1
-            #               n.parent = current
-            #               Q.enqueue(n)
-
-    return "SSSSSEENNNEEEEEEESSWWWWSSSEEEESS"
-
-def get_adjacent_nodes(lab, x, y):
-    """
-    Given indices x and y, determine which adjacent elements in the matrix are valid paths
-    :param x: The row of the matrix
-    :param y: The column of the matrix
-    :return:
-    """
-    adjacent_nodes = []
-    if lab[x+1][y] == 0:
-        adjacent_nodes.append(lab[x+1][y])
-    if lab[x-1][y] == 0:
-        adjacent_nodes.append(lab[x-1][y])
-    if lab[x][y-1] == 0:
-        adjacent_nodes.append(lab[x][y-1])
-    if lab[x][y+1] == 0:
-        adjacent_nodes.append(lab[x][y+1])
-    return adjacent_nodes
+    final_path = ''
+    for d in path:
+        final_path += d
+    return final_path
 
 
 if __name__ == '__main__':
-    #This code using only for self-checking and not necessary for auto-testing
+    # This code using only for self-checking and not necessary for auto-testing
     def check_route(func, labyrinth):
         MOVE = {"S": (1, 0), "N": (-1, 0), "W": (0, -1), "E": (0, 1)}
-        #copy maze
+        # copy maze
         route = func([row[:] for row in labyrinth])
         pos = (1, 1)
         goal = (10, 10)
         for i, d in enumerate(route):
             move = MOVE.get(d, None)
             if not move:
-                print("Wrong symbol in route")
                 return False
             pos = pos[0] + move[0], pos[1] + move[1]
             if pos == goal:
                 return True
             if labyrinth[pos[0]][pos[1]] == 1:
-                print("Player in the pit")
                 return False
-        print("Player did not reach exit")
         return False
+
 
     # These assert are using only for self-testing as examples.
     assert check_route(checkio, [
@@ -109,6 +85,7 @@ if __name__ == '__main__':
         [1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1],
         [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "First maze"
+
     assert check_route(checkio, [
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
